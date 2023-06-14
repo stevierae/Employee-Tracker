cee1040dd069f8b2427bcbb2d37b2b272978ac66
Stefanie Fisher <stefaniefisher@Stefanies-Air.fios-router.home>
2023-06-13 20:22:27 -0400

    README update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Employee-Tracker
+
+ [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  In this project we used MySQL (Structured Query Language) to create different tables to store and manage data.  We've created an Employee Management System (EMS) to enter, access and edit data on employees, departments and their roles. 
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+  To use this application, from the command line type in 'npm i' to download all necessary packages then run 'node index' to see your menu of options.
--- index.js
+++ index.js
@@ -13,3 +13,79 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employeetracker database.`)
   );
+
+  .then((connection) => {
+    db = connection;
+    console.log('Connected to the human_resources_db database.');
+    init();
+  });
+
+// create body of table
+  const config ={
+    border: {
+        topBody: `─`,
+      topJoin: `┬`,
+      topLeft: `┌`,
+      topRight: `┐`,
+  
+      bottomBody: `─`,
+      bottomJoin: `┴`,
+      bottomLeft: `└`,
+      bottomRight: `┘`,
+  
+      bodyLeft: `│`,
+      bodyRight: `│`,
+      bodyJoin: `│`,
+  
+      joinBody: `─`,
+      joinLeft: `├`,
+      joinRight: `┤`,
+      joinJoin: `┼`
+    }
+  };
+
+  async function showTable (data){
+    let tableData = [];
+    tableData = [
+        Object.keys(data[0]),
+        ...data.map(val => Object.values(val))];
+        console.log(tableData);
+        const answers = await inquirer.prompt([
+          {
+            message: "\n" + table(tableData, config),
+            type: 'input',
+            name: 'name'
+          }
+        ]);
+  }
+
+//   Function that takes in data and turns it into an array of objects
+  const menu = async function (){
+    const choices = await inquirer.prompt([
+        {
+            message: "What do you want to do?",
+            type: "list",
+            name: "options",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Update employee managers",
+                "Delete departments, roles, and employees",
+                "View the total utilized budget of a department",
+            ]
+
+        }
+    ])
+  }
+
+  if(choices.options === "View all departments"){
+    await viewDepartments();
+    await menu();
+  } else if(choices.options === "View all roles"){
+
+  }
